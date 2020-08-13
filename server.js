0288cc895dd2666e0da04f16eb00f62558413f4f
Vish Singh <vishalicious213@gmail.com>
2020-08-13 16:19:49 -0700

    req variables added to logger middleware

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-  console.log('Request method: ');
-  console.log('Request URL: ');
-  console.log('Timestamp: ');
+  console.log('Request method: ', req.method);
+  console.log('Request URL: ', req.url);
+  console.log('Timestamp: ', new Date().toISOString());
   next();
 }
 
