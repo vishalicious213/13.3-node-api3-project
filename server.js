9589f1a58e4836617de53b659bbd0da26ce605fe
Vish Singh <vishalicious213@gmail.com>
2020-08-13 21:41:28 -0700

    drafted validateUserId

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const userDb = require('./users/userDb');
 
 const server = express();
 
@@ -13,6 +14,18 @@ function logger(req, res, next) {
   console.log('Request URL: ', req.url);
   console.log('Timestamp: ', new Date().toISOString());
   next();
+}
+
+function validateUserId(req, res, next) {
+  userDb.getById(req.params.id)
+    .then(user => {
+      if(!user) {
+        res.status(400).json({ message: 'Invalid user ID' });
+      } else {
+        req.user = user;
+        next();
+      }
+    });
 }
 
 app.use(logger);
